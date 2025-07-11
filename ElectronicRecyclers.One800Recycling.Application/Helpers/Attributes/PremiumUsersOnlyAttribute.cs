b49d4bf4a3625c00a5965bf6f067ad146a58a4c7
deepak sharma <deepaksharma@cssoftsolutions.com>
2025-07-11 11:36:36 +0530

    Initial Git changes

---
+++ ElectronicRecyclers.One800Recycling.Application/Helpers/Attributes/PremiumUsersOnlyAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectronicRecyclers.One800Recycling.Web.Helpers.Attributes
+{
+    public class PremiumUsersOnlyAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext == null)
+                throw new ArgumentNullException("httpContext");
+
+            var user = httpContext.User;
+            return user.IsInRole("Administrator") || user.IsInRole("Power User");
+        }
+    }
+}
